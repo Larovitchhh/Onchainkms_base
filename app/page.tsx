3915ce21c277af8a852d55821547cfeb77de4444
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-07 12:14:58 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import ActivityForm from "../components/ActivityForm"
+
+export default function Home() {
+
+  return (
+    <main>
+
+      <h1>Onchain Sports Tracker</h1>
+
+      <p>
+        Register your activity and mint it on Base
+      </p>
+
+      <ActivityForm />
+
+    </main>
+  )
+}
