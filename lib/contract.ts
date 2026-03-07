fa9b041da13301f093ef87307ea610b03bf07f03
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-07 12:11:36 +0100

    Create contract.ts

---
+++ lib/contract.ts
@@ -0,0 +1,17 @@
+export const contractAddress = "0x03c1539C6886c07166d33B1b2D80B66852253Aa1"
+
+export const contractABI = [
+ {
+  name:"mintActivity",
+  type:"function",
+  stateMutability:"nonpayable",
+  inputs:[
+   {name:"user",type:"address"},
+   {name:"xp",type:"uint256"},
+   {name:"kms",type:"uint256"},
+   {name:"duration",type:"uint256"},
+   {name:"elevation",type:"uint256"}
+  ],
+  outputs:[]
+ }
+]
