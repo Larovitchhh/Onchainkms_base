c6b2ceb12b65cb1c0921234ceef3afd67430ad16
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-07 12:29:08 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import "./globals.css"
+
 export const metadata = {
   title: "Onchain Sports",
   description: "Mint your sport activity on Base"
@@ -10,7 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body style={{fontFamily:"Arial",padding:"40px"}}>
+      <head>
+        <meta
+          name="talentapp:project_verification"
+          content="7484bc8aab7a80329d9a68fc559aad005ce48622f2bdea6131807097a49770a9c78661ef758841b68a2265853274175de292dff70ca314ced5dbb4232fd6d140"
+        />
+      </head>
+
+      <body>
         {children}
       </body>
     </html>
