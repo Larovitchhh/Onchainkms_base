4eab19ff2a052b6a1bc62ffce8506f86e4854cd1
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-09 22:01:58 +0100

    Update contract.ts

--- lib/contract.ts
+++ lib/contract.ts
@@ -1,17 +1,17 @@
-export const CONTRACT_ADDRESS = "0x03c1539C6886c07166d33B1b2D80B66852253Aa1"
-
 export const CONTRACT_ABI = [
  {
   name:"mintActivity",
   type:"function",
   stateMutability:"nonpayable",
   inputs:[
-   {name:"user",type:"address"},
+   {name:"to",type:"address"},
+   {name:"km",type:"uint256"},
    {name:"xp",type:"uint256"},
-   {name:"kms",type:"uint256"},
-   {name:"duration",type:"uint256"},
-   {name:"elevation",type:"uint256"}
+   {name:"stravaId",type:"string"},
+   {name:"metadataURI",type:"string"}
   ],
-  outputs:[]
+  outputs:[
+   {name:"",type:"uint256"}
+  ]
  }
 ]
