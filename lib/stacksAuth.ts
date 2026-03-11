ae5254339de9ddc9d1283bbfac2da5b8b9e14f0f
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-11 14:14:31 +0100

    Update stacksAuth.ts

--- lib/stacksAuth.ts
+++ lib/stacksAuth.ts
@@ -1,7 +1,25 @@
-import { AppConfig, UserSession } from "@stacks/connect"
+import { AppConfig, UserSession, showConnect } from "@stacks/connect"
 
 const appConfig = new AppConfig(["store_write"])
 
 export const userSession = new UserSession({
  appConfig
 })
+
+export function connectStacks(){
+
+ showConnect({
+
+  appDetails:{
+   name:"Onchain Sports",
+   icon: window.location.origin + "/favicon.ico"
+  },
+
+  userSession,
+
+  onFinish:()=>{
+   console.log("Stacks wallet connected")
+  }
+
+ })
+}
