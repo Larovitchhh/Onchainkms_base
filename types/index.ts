2f4608635c29434c5d5ab78b4e155bff89e653b4
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-15 22:20:58 +0100

    Update index.ts

--- types/index.ts
+++ types/index.ts
@@ -11,8 +11,16 @@ export interface ActivityMetadata {
   name: string;
   description: string;
   image: string;
+  external_url?: string;
   attributes: {
     trait_type: string;
     value: string | number;
   }[];
+}
+
+export interface SportDisplay {
+  id: SportType;
+  label: string;
+  icon: string;
+  color: string;
 }
