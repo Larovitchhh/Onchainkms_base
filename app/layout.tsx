b3279e1b2355c322a8344809cc963f15c88b28d3
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-03-17 23:01:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,7 @@ import "./globals.css"
 
 export const metadata = {
   title: "Onchain Sports",
-  description: "Mint your sport activity on Base",
-  // Intentamos inyectar el CSP vía Metadata de Next.js
-  other: {
-    'content-security-policy': "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com; img-src 'self' data: https: blob:; style-src 'self' 'unsafe-inline';",
-  },
+  description: "Mint your sport activity on Base"
 }
 
 export default function RootLayout({
@@ -17,11 +13,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Etiqueta META física: Esta es la que suele mandar sobre el resto */}
-        <meta 
-          httpEquiv="Content-Security-Policy" 
-          content="default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; img-src 'self' data: https: blob:; style-src 'self' 'unsafe-inline';" 
-        />
         <meta
           name="talentapp:project_verification"
           content="7484bc8aab7a80329d9a68fc559aad005ce48622f2bdea6131807097a49770a9c78661ef758841b68a2265853274175de292dff70ca314ced5dbb4232fd6d140"
