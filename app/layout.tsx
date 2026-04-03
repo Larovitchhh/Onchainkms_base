bc495bf4825b4064dbfa01a82a803802682bc1a6
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-04-03 20:38:28 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,13 +23,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        {/* ID de tu proyecto en Base.dev */}
         <meta name="base:app_id" content="6941e8fdd19763ca26ddc370" />
-        
-        {/* Cargamos el SDK de Farcaster v2 por CDN para evitar el comando 'npm install' */}
-        <script src="https://cdn.jsdelivr.net/npm/@farcaster/frame-sdk/dist/bundle.js"></script>
-        
-        <meta name="talentapp:project_verification" content="7484bc8aab7a80329d9a68fc559aad005ce48622f2bdea6131807097a49770a9c78661ef758841b68a2265853274175de292dff70ca314ced5dbb4232fd6d140" />
+        <script src="https://cdn.jsdelivr.net/npm/@farcaster/frame-sdk/dist/bundle.js" defer></script>
         <link rel="icon" href="/nft/favicon.png" />
       </head>
       <body>{children}</body>
