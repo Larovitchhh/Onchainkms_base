be7845321a5247e5fa7965d89e3e79b839060917
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-04-06 23:56:37 +0200

    Update ConnectStacks.tsx

--- components/ConnectStacks.tsx
+++ components/ConnectStacks.tsx
@@ -2,34 +2,26 @@
 
 import { connectStacks } from "../lib/stacksAuth"
 
-export default function ConnectStacks() {
-  async function handleClick() {
-    console.log("CLICK EN CONNECT STACKS")
-    try {
-      await connectStacks()
-      console.log("connectStacks terminó")
-    } catch (err) {
-      console.error("Error en connectStacks:", err)
-      alert("Error conectando Stacks wallet")
-    }
-  }
+export default function ConnectStacks(){
 
-  return (
-    <button
-      type="button"
-      onClick={handleClick}
-      style={{
-        padding:"10px 20px",
-        background:"#5546ff",
-        color:"white",
-        border:"none",
-        borderRadius:"8px",
-        cursor:"pointer",
-        fontWeight:"bold",
-        marginBottom:"20px"
-      }}
-    >
-      Connect Stacks Wallet
-    </button>
-  )
+ return(
+
+  <button
+   onClick={connectStacks}
+   style={{
+    padding:"10px 20px",
+    background:"#5546ff",
+    color:"white",
+    border:"none",
+    borderRadius:"8px",
+    cursor:"pointer",
+    fontWeight:"bold",
+    marginBottom:"20px"
+   }}
+  >
+   Connect Stacks Wallet
+  </button>
+
+ )
+
 }
