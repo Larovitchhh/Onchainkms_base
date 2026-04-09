a0f1297f8434d11ca04f1bbd585ea8f5fe913ae0
Larovitchhh <115192338+Larovitchhh@users.noreply.github.com>
2026-04-09 11:58:29 +0200

    Update stacksAuth.ts

--- lib/stacksAuth.ts
+++ lib/stacksAuth.ts
@@ -1,25 +1,24 @@
-import { AppConfig, UserSession, showConnect } from "@stacks/connect"
+import { AppConfig, UserSession, showConnect } from "@stacks/connect";
 
-const appConfig = new AppConfig(["store_write"])
+const appConfig = new AppConfig(["store_write"]);
 
 export const userSession = new UserSession({
- appConfig
-})
+  appConfig
+});
 
-export function connectStacks(){
+export function connectStacks() {
+  // Verificamos que estamos en el cliente antes de usar window
+  const origin = typeof window !== "undefined" ? window.location.origin : "";
 
- showConnect({
-
-  appDetails:{
-   name:"Onchain Sports",
-   icon: window.location.origin + "/favicon.ico"
-  },
-
-  userSession,
-
-  onFinish:()=>{
-   console.log("Stacks wallet connected")
-  }
-
- })
+  showConnect({
+    appDetails: {
+      name: "Onchain Sports",
+      icon: origin + "/favicon.ico",
+    },
+    userSession,
+    onFinish: () => {
+      console.log("Stacks wallet connected");
+      window.location.reload(); // Recargamos para limpiar cualquier residuo de sesión antigua
+    },
+  });
 }
